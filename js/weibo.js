6a67e9d859edfa05b901ba5336cc93142d228474
RayJiang <jianghui@dxy.cn>
2024-05-22 14:07:52 +0800

    test add rule

--- js/weibo.js
+++ js/weibo.js
@@ -2,15 +2,6 @@
 应用名称：自用微博国际版去广告脚本
 */
 
-var body = $response.body;
+var body = "{}";
 var url = $request.url;
-if (url.includes("/ad")) {
-    body = "";
-}
-if (url.includes("get_coopen_ads")) {
-    body = "";
-}
-if (url.includes("=ad")) {
-    body = "";
-}
 $done({body});
